c48b9c47dcd85c3928db54c8a93c50c5fc575fd1
DSastre <420352+DSastre@users.noreply.github.com>
2020-04-10 21:45:04 +0200

    Refactor prepareData(), add getters and setters for CALC and DOC

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@
 **************************************/
 
 
-const calc = {
+const CALC = {
     _weighting: [7, 3, 1],
     _values: { 
         _digits: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -19,41 +19,68 @@ const calc = {
         },
     },
     get values() {
-        return this._values._digits.concat(Object.keys(this._values._abc));
+        const digitsAsStrings = [];
+        this._values._digits.forEach(element => {
+            digitsAsStrings.push(element.toString());
+        });
+        return digitsAsStrings.concat(Object.keys(this._values._abc));
     },
     msg: { 
-        empty: `> Element didn't contain any symbols and has been filled with '<'.\r\n`,
-        exceed: `> Element exceeded the maximum length of 12 symbols and has been trimmed.\r\n`,
-        invalid: [`> Invalid symbol '`, `' at index `, ` has been replaced with '<'.\r\n`]
+        emptyError: `> Element didn't contain any symbols and has been filled with '<'.\r\n`,
+        exceedError: `> Element exceeded the maximum length of 12 symbols and has been trimmed.\r\n`,
+        _invalidError: [`> Invalid symbol '`, `' at index `, ` has been replaced with '<'.\r\n`],
+        _rawLog: [`> Raw input: '`, `'\r\n`],
+        _preparedLog: [`> Prepared string for further calculation: '`, `'\r\n`],
+        invalidError(symbol, index) {
+            return this._invalidError[0] + symbol + 
+            this._invalidError[1] + index + 
+            this._invalidError[2];
+        },
+        rawLog(testingString) {
+            return this._rawLog[0] + testingString + this._rawLog[1];
+        },
+        preparedLog(testingString) {
+            return this._preparedLog[0] + testingString + this._preparedLog[1];
+        }
     },
     result: ''
 }
 
-const doc = {
-    errorMsg: document.getElementById('error'),
-    logMsg: document.getElementById('log'),
+
+const DOC = {
+    _errorField: document.getElementById('error'),
+    _logField: document.getElementById('log'),
+    set errorField(errorMessage) {
+        if (this._errorField.childNodes.length == 0) {
+            this._errorField.textContent = errorMessage;
+        } else {
+            this._errorField.textContent += errorMessage;
+        }
+    },
+    set logField(logMessage) {            
+        if (this._logField.childNodes.length == 0) {
+            this._logField.textContent = logMessage;
+        } else {
+            this._logField.textContent += logMessage;
+        } 
+    },
     rButton_onlyDigit: document.getElementById('checkdigit'),
     inputField: document.getElementById('input-field'),
     resultField: document.getElementById('result-field'),
     submitButton: document.getElementById('submit-button')
 }
 
-//console.log(calc.values);
 
-const prepareData = testingString => {
-    console.log('raw input: ' + testingString);
-    //doc.logMsg.textContent = '> raw input: ' + testingString;
-
-    const convertedArray = testingString;
+CALC.prepareData = testingString => {
+    DOC.logField = CALC.msg.rawLog(testingString);
+    // convert all letters to uppercase-notation
+    testingString = testingString.toUpperCase();
+    // handle cases with no input
     if (testingString.length == 0) {
-        // handle input strings with no length
         testingString = '<<<<<<<<<<<<';
-        doc.errorMsg.textContent += calc.msg.empty;
-    } else {
-        // convert all letters to uppercase-notation
-        testingString = testingString.toUpperCase();
+        DOC.errorField = CALC.msg.emptyError;
     };
-    // convert strings to arrays
+    // convert string to arrays
     testingString = Array.from(testingString);
     if (testingString.length <= 12) {
         // fill all elements with less than 12 positions with '<'
@@ -62,34 +89,27 @@ const prepareData = testingString => {
     } else {
         // trim elements with more than 12 positions
         testingString.splice(12);
-        doc.errorMsg.textContent += calc.msg.exceed;
+        DOC.errorField = CALC.msg.exceedError;
     };
     // fill all spaces between arrays with '<'
-    for (let i = 0; i < 12; i++) {
-        if (testingString[i] == ' ') {
-            testingString[i] = '<';
-        }
-    };
+    testingString.forEach(element => {
+        if (element == ' ') { element = '<'}
+    });
     // change invalid symbols to '<'
     for (let i = 0; i < 12; i++) {
-        if (
-            Object.keys(calc._values._abc).includes(testingString[i]) == false 
-            && calc._values._digits.includes(parseInt(testingString[i])) == false) { 
-                doc.errorMsg.textContent += 
-                    calc.msg.invalid[0] + testingString[i] 
-                  + calc.msg.invalid[1] + i 
-                  + calc.msg.invalid[2];
-                testingString[i] = '<';
-            } 
+        if (CALC.values.includes(testingString[i]) == false) { 
+            DOC.errorField = CALC.msg.invalidError(testingString[i], i);
+            testingString[i] = '<';
+        } 
     };
-    calc.result = testingString.join('');
-    console.log('prepared string for further calculation: ' + testingString);
+    CALC.result = testingString.join('');
+    DOC.logField = CALC.msg.preparedLog(testingString);
     return testingString;
 }
 
 
 const convertInputToNumbers = inputString => {
-    const preparedData = prepareData(inputString);
+    const preparedData = CALC.prepareData(inputString);
     
     for (let i = 0; i < 12; i++) {
         if (parseInt(preparedData[i]) >= 0) {
@@ -97,7 +117,7 @@ const convertInputToNumbers = inputString => {
             preparedData[i] = parseInt(preparedData[i]);
         } else {
             // convert alphabetical symbols and '<' to their respective nrs
-            preparedData[i] = calc._values._abc[preparedData[i]];
+            preparedData[i] = CALC._values._abc[preparedData[i]];
         } 
     };
     console.log('input converted in numbers: ' + preparedData)
@@ -112,7 +132,7 @@ const createCheckDigit = inputString => {
         
     // step 1: multiply each digit with corresponding wighting nr.
     for (let j = 0; j<12; j++) {
-        inputAsNumbers[j] = inputAsNumbers[j] * calc._weighting[j%3];
+        inputAsNumbers[j] = inputAsNumbers[j] * CALC._weighting[j%3];
     };
     console.log('step 1) multiplication results: ' + inputAsNumbers);
     // step 2: add the products from step 1
@@ -123,9 +143,9 @@ const createCheckDigit = inputString => {
     // step 3 & 4: Divide by ten and keep reminder
     inputAsNumbers = inputAsNumbers % 10;
     console.log('step 3 & 4) reminder of division by 10: ' + inputAsNumbers);
-    calc.result = [calc.result, '<<<', inputAsNumbers].join('');
-    console.log('result with check digit: ' + calc.result);
-    return calc.result;
+    CALC.result = [CALC.result, '<<<', inputAsNumbers].join('');
+    console.log('result with check digit: ' + CALC.result);
+    return CALC.result;
 }
 
 
@@ -136,7 +156,7 @@ const createCheckLetter = inputString => {
     let letter
     // step 1: multiply each digit with corresponding wighting nr.
     for (let j = 0; j<12; j++) {
-        inputAsNumbers[j] = inputAsNumbers[j] * calc._weighting[j%3];
+        inputAsNumbers[j] = inputAsNumbers[j] * CALC._weighting[j%3];
     };
     console.log('step 1) multiplication results: ' + inputAsNumbers);
     // step 2: add the products from step 1
@@ -151,28 +171,30 @@ const createCheckLetter = inputString => {
     if (inputAsNumbers < 10) {
         letter = inputAsNumbers;
     } else {
-        letter = Object.entries(calc._values._abc)[inputAsNumbers - 10][0];
+        letter = Object.entries(CALC._values._abc)[inputAsNumbers - 10][0];
     }
 
-    calc.result = [calc.result, '<<<', letter].join('');
+    CALC.result = [CALC.result, '<<<', letter].join('');
 
-    console.log('result with check letter: ' + calc.result);
-    return calc.result;
+    console.log('result with check letter: ' + CALC.result);
+    return CALC.result;
 }
 
 
-doc.submitButton.onclick = () => {
+DOC.submitButton.onclick = () => {
     
     // this is necessary to use \r\n in textContent commands
-    doc.errorMsg.setAttribute('style', 'white-space: pre-line;');
+    DOC._errorField.setAttribute('style', 'white-space: pre-line;');
+    DOC._logField.setAttribute('style', 'white-space: pre-line;');
     
     // reset error messages with every new request
-    doc.errorMsg.textContent = '';
-    if (doc.rButton_onlyDigit.checked) {
-        let checkDigitResult = createCheckDigit(doc.inputField.value);
-        doc.resultField.setAttribute('placeholder', checkDigitResult);
+    DOC._errorField.textContent = '';
+    DOC._logField.textContent = '';
+    if (DOC.rButton_onlyDigit.checked) {
+        let checkDigitResult = createCheckDigit(DOC.inputField.value);
+        DOC.resultField.setAttribute('placeholder', checkDigitResult);
     } else {
-        let checkLetterResult = createCheckLetter(doc.inputField.value);
-        doc.resultField.setAttribute('placeholder', checkLetterResult);
+        let checkLetterResult = createCheckLetter(DOC.inputField.value);
+        DOC.resultField.setAttribute('placeholder', checkLetterResult);
     }
 };
