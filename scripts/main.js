b911c32f2f6c0b740c21b4fdf0dda0a77f9b87d0
DSastre <420352+DSastre@users.noreply.github.com>
2020-04-12 12:46:32 +0200

    Minor code clean-up

--- scripts/main.js
+++ scripts/main.js
@@ -84,9 +84,7 @@ CALC.prepareData = inputString => {
     let upperCaseString = inputString.toUpperCase();
     if (upperCaseString.length == 0) {
         upperCaseString = '<<<<<<<<<<<<';
-        CALC.error(
-            `> Element didn't contain any symbols and has been filled with '<'.\r\n`
-        );
+        CALC.error(`> Element didn't contain any symbols and has been filled with '<'.\r\n`);
     };    
     let convertedToArray = Array.from(upperCaseString);
     if (convertedToArray.length <= 12) { 
@@ -94,25 +92,19 @@ CALC.prepareData = inputString => {
         convertedToArray = convertedToArray.concat(fillTail);
     } else { 
         convertedToArray.splice(12);
-        CALC.error(
-            `> Element exceeded the maximum length of 12 symbols and has been trimmed.\r\n`
-        );
+        CALC.error(`> Element exceeded the maximum length of 12 symbols and has been trimmed.\r\n`);
     };
     convertedToArray.forEach(element => {
         if (element == ' ') { element = '<'}
     });
     for (let i = 0; i < 12; i++) {
         if (CALC.consideredValues.includes(convertedToArray[i]) == false) { 
-            CALC.error(
-                `> Invalid symbol '${convertedToArray[i]}' at index ${i} has been replaced with '<'.\r\n`
-            );
+            CALC.error(`> Invalid symbol '${convertedToArray[i]}' at index ${i} has been replaced with '<'.\r\n`);
             convertedToArray[i] = '<';
         } 
     };
     CALC.result = convertedToArray.join('');
-    CALC.log(
-        `> Prepared string for further calculation: '${convertedToArray}'\r\n`
-    );
+    CALC.log(`> Prepared string for further calculation: '${convertedToArray}'\r\n`);
     return convertedToArray;
 }
 
@@ -127,9 +119,7 @@ CALC.transformInputToNumbers = inputString => {
             transformedToNumbers[i] = CALC._consideredValues._abc[preparedData[i]];
         } 
     };
-    CALC.log(
-        `> Input transformed into numbers: ${transformedToNumbers}\r\n`
-    )
+    CALC.log(`> Input transformed into numbers: ${transformedToNumbers}\r\n`)
     return transformedToNumbers;
 }
 
@@ -172,10 +162,10 @@ CALC.createCheckChar = (inputString, choise) => {
 
 
 DOC.submitButton.onclick = () => {
-    DOC._errorField.setAttribute('style', 'white-space: pre-line;');    // to use \r\n in textContent commands
-    DOC._logField.setAttribute('style', 'white-space: pre-line;');      // to use \r\n in textContent commands
-    DOC._errorField.textContent = '';   // reset error messages with every new request
-    DOC._logField.textContent = '';     // reset error messages with every new request
+    DOC._errorField.setAttribute('style', 'white-space: pre-line;');
+    DOC._logField.setAttribute('style', 'white-space: pre-line;');
+    DOC._errorField.textContent = '';
+    DOC._logField.textContent = '';
     if (DOC.rButton_onlyDigit.checked) {
         let checkDigitResult = CALC.createCheckChar(DOC.inputField.value, 'number');
         DOC.resultField.setAttribute('placeholder', checkDigitResult);
